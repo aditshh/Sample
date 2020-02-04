e19096243e24b498b5c2a63a2570097293ed8a03
Adit Shah <aditshh@gmail.com>
2020-02-04 11:52:12 -0500

    Async implementation of Create, Put, Get, Post

--- HttpClientSample/HttpClientSample/Program.cs
+++ HttpClientSample/HttpClientSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿#region snippet_all
+
 using System;
 using System.Net;
 using System.Net.Http;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 namespace HttpClientSample
 {
     #region snippet_prod
+
     public class Product
     {
         public string Id { get; set; }
@@ -15,65 +17,76 @@ namespace HttpClientSample
         public decimal Price { get; set; }
         public string Category { get; set; }
     }
+
     #endregion
 
     class Program
     {
         #region snippet_HttpClient
-        static HttpClient client = new HttpClient();
+
+        private static readonly HttpClient Client = new HttpClient();
+
         #endregion
 
         static void ShowProduct(Product product)
         {
             Console.WriteLine($"Name: {product.Name}\tPrice: " +
-                $"{product.Price}\tCategory: {product.Category}");
+                              $"{product.Price}\tCategory: {product.Category}");
         }
 
         #region snippet_CreateProductAsync
+
         static async Task<Uri> CreateProductAsync(Product product)
         {
-            HttpResponseMessage response = await client.PostAsJsonAsync(
+            HttpResponseMessage response = await Client.PostAsJsonAsync(
                 "api/products", product);
             response.EnsureSuccessStatusCode();
 
             // return URI of the created resource.
             return response.Headers.Location;
         }
+
         #endregion
 
         #region snippet_GetProductAsync
+
         static async Task<Product> GetProductAsync(string path)
         {
             Product product = null;
-            HttpResponseMessage response = await client.GetAsync(path);
+            HttpResponseMessage response = await Client.GetAsync(path);
             if (response.IsSuccessStatusCode)
             {
                 product = await response.Content.ReadAsAsync<Product>();
             }
+
             return product;
         }
+
         #endregion
 
         #region snippet_UpdateProductAsync
-        static async Task<Product> UpdateProductAsync(Product product)
+
+        static async Task UpdateProductAsync(Product product)
         {
-            HttpResponseMessage response = await client.PutAsJsonAsync(
+            HttpResponseMessage response = await Client.PutAsJsonAsync(
                 $"api/products/{product.Id}", product);
             response.EnsureSuccessStatusCode();
 
             // Deserialize the updated product from the response body.
-            product = await response.Content.ReadAsAsync<Product>();
-            return product;
+            await response.Content.ReadAsAsync<Product>();
         }
+
         #endregion
 
         #region snippet_DeleteProductAsync
+
         static async Task<HttpStatusCode> DeleteProductAsync(string id)
         {
-            HttpResponseMessage response = await client.DeleteAsync(
+            HttpResponseMessage response = await Client.DeleteAsync(
                 $"api/products/{id}");
             return response.StatusCode;
         }
+
         #endregion
 
         static void Main()
@@ -82,14 +95,17 @@ namespace HttpClientSample
         }
 
         #region snippet_run
+
         #region snippet5
+
         static async Task RunAsync()
         {
             // Update port # in the following line.
-            client.BaseAddress = new Uri("http://localhost:64195/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
+            Client.BaseAddress = new Uri("http://localhost:65316/");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
+
             #endregion
 
             try
@@ -121,7 +137,6 @@ namespace HttpClientSample
                 // Delete the product
                 var statusCode = await DeleteProductAsync(product.Id);
                 Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
             }
             catch (Exception e)
             {
@@ -130,7 +145,9 @@ namespace HttpClientSample
 
             Console.ReadLine();
         }
+
         #endregion
     }
 }
+
 #endregion
